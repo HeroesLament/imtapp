f24d9e69cbc93c57108333882ea23fdd5ebbf6eb
HeroesLament <mwynkoop@siliconiq.com>
2023-12-09 14:47:31 -0900

    Capturing progress

--- src/IncidentManagement/BackendCloseIncident.js
+++ src/IncidentManagement/BackendCloseIncident.js
@@ -61,7 +61,13 @@ function completeIncident(incidentFolderId, endDate) {
         console.log("Incident Sucessfully Closed");
 
       }
-      SystemTools.forceUpdate();
+      SystemTools.forceUpdate(); // Update the Filter Mapper
+      // Remove Incident Waypoints from Incident Mapper Spreadsheet
+      const result = incidentMapperUpdate(incidentName);
+      const rowIndexes = findRowsForIncident(incidentName);
+      const deletionRequests = createDeletionRequests(rowIndexes);
+      const response = batchUpdate(SystemSettings.SPOT_INCIDENT_MAPPER_ID, deletionRequests);
+      
       var msg = [true, incidentFolderId, incidentName];
       return msg;
     }
@@ -72,3 +78,86 @@ function completeIncident(incidentFolderId, endDate) {
   }
 }
 
+function findRowsForIncident(incidentName, sheet) {
+  const sheet = SpreadsheetApp.openById(SystemSettings.SPOT_INCIDENT_MAPPER_ID).getSheets()[1]; // Open Incident Mapper Spreadsheet
+  const dataRange = sheet.getRange('C11:C1000').getValues(); // Creates an array of RowData enumerating the Folder/Incident name
+  let rowIndexes = [];
+
+  for (let i = 0; i < dataRange.length; i++) {
+    if (dataRange[i][0].includes(incidentName)) {
+      rowIndexes.push(i + 10); // Adjust the index to match sheet rows
+    }
+  }
+
+  return rowIndexes;
+}
+
+function createDeletionRequests(rowIndexes) {
+  var requests = [];
+  rowIndexes.forEach(rowId => {
+    requests.push(createUpdateCellsRequest(rowId));
+  });
+  return requests;
+}
+
+function batchUpdate(spreadsheetId, requests) {
+  var url = 'https://sheets.googleapis.com/v4/spreadsheets/' + spreadsheetId + ':batchUpdate';
+  var payload = {
+    requests: requests
+  };
+  
+  var options = {
+    method: 'post',
+    contentType: 'application/json',
+    headers: {
+      'Authorization': 'Bearer ' + ScriptApp.getOAuthToken()
+    },
+    // Convert the JavaScript object to a JSON string.
+    payload: JSON.stringify(payload),
+    muteHttpExceptions: true
+  };
+  
+  var response = UrlFetchApp.fetch(url, options);
+  Logger.log(response.getContentText());
+}
+
+function shiftRowsUp(sheet, startRow, endRow, numRowsShifted) {
+  // Read data into a JavaScript array (in-memory)
+  var range = sheet.getRange(startRow + 1, 1, endRow - startRow, sheet.getLastColumn());
+  var data = range.getValues();
+
+  // Manipulate the array to remove specific rows (if needed)
+  // For example, if rows 11, 14, 19 are removed, adjust the data array accordingly
+
+  // Write the data back to the sheet, shifted up
+  sheet.getRange(startRow - numRowsShifted + 1, 1, data.length, data[0].length).setValues(data);
+
+  // Optionally, clear any remaining rows that are now empty due to the shift
+  var rowsToClear = endRow - (startRow - numRowsShifted + 1 + data.length);
+  if (rowsToClear > 0) {
+    sheet.getRange(endRow - rowsToClear + 1, 1, rowsToClear, sheet.getLastColumn()).clearContent();
+  }
+}
+
+function incidentMapperUpdate(incidentName) {
+  try {
+    const sheet = SpreadsheetApp.openById(SystemSettings.SPOT_INCIDENT_MAPPER_ID).getSheets()[1];
+    const rowIndexes = findRowsForIncident(incidentName, sheet);
+    let requests = createDeletionRequests(rowIndexes);
+
+    // Calculate the number of rows to be shifted and add shift requests
+    const numRowsShifted = rowIndexes.length;
+    const shiftRequests = createShiftRequests(sheet, numRowsShifted, rowIndexes);
+    requests = requests.concat(shiftRequests);
+
+    // Perform the batch update
+    const response = batchUpdate(SystemSettings.SPOT_INCIDENT_MAPPER_ID, requests);
+
+    var msg = [true, incidentName];
+    return msg;
+  } catch (error) {
+    console.log("Error in incidentMapperUpdate for " + incidentName + ": " + error);
+    var msg = [false, error.toString()];
+    return msg;
+  }
+}
---
+++ src/IncidentManagement/tests.js
@@ -0,0 +1,5 @@
+function testCompleteIncident(){
+    const incidentFolderId = '1jMAucwXRB9ABlODK3LbvXjMYgc3hSCU3'
+    const endDate = 'December 2, 2023'
+    completeIncident(incidentFolderId, endDate)
+  }
