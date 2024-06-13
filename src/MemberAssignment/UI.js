5c17543ff65f7872d4e9c397e8cdb568878346f1
HeroesLament <mwynkoop@siliconiq.com>
2024-06-13 08:36:32 -0800

    Revert UI.js

--- src/MemberAssignment/UI.js
+++ src/MemberAssignment/UI.js
@@ -1,24 +1,5 @@
 function doGet(request) {
-    // Check if any action parameters are provided
-    if (request.parameter.action) {
-        const headers = {
-            'Access-Control-Allow-Origin': '*'
-        };
-        // Route to the API handler if an action parameter is present
-        return router(request);
-    } else {
-        const headers = {
-            'Access-Control-Allow-Origin': '*', // Adjust according to your security policies
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-        };
-        // Serve the default HTML page if no parameters are specified
-        return HtmlService.createTemplateFromFile('Page')
-                          .evaluate()
-                          .setTitle("KVRS IMS - Member Assignment Module")
-                          // Unsure if Headers statement will cause issues; remove if problematic
-                          .setHeaders(headers);
-    }
+    return HtmlService.createTemplateFromFile('Page').evaluate().setTitle("KVRS IMS - Member Assignment Module");
 }
 
 function include(filename) {
